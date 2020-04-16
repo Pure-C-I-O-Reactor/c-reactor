609df3693628681eb092cae62362ecd1d994b869
aseke7182 <a>
2020-04-16 16:52:53 +0600

    ADD: doc. for reactor's interface

--- reactor.h
+++ reactor.h
@@ -4,12 +4,20 @@
 #include <stdint.h>
 #include <time.h>
 
+// Pointer to the function, that is called by I/O reactor when event received from sys.selector
 typedef void (*Callback)(void *arg, int fd, uint32_t events);
 
+// Creating new struct of Reactor
 typedef struct reactor Reactor;
 
+// If we receive NULL - some error
+// Otherwise pointer to our reactor
 Reactor *reactor_new(void);
 
+/*
+	All below methods are response -1 if some error and 0 in case of success.
+	They are responsible for reactor's functionality. 
+*/
 int reactor_destroy(Reactor *reactor);
 
 int reactor_register(const Reactor *reactor, int fd, uint32_t interest,
@@ -17,6 +25,12 @@ int reactor_register(const Reactor *reactor, int fd, uint32_t interest,
 int reactor_deregister(const Reactor *reactor, int fd);
 int reactor_reregister(const Reactor *reactor, int fd, uint32_t interest,
                        Callback callback, void *callback_arg);
+
+/*
+	Here we are starting our reactor with timer timeout.
+	This function will give control to the calling 
+	 if the allotted time has elapsed or an error of sockets.
+*/
 int reactor_run(const Reactor *reactor, time_t timeout);
 
 #endif 
